88cc48f116b6c3e86d3a32762e9ce395538d8d61
jovuvu <jonathan.vu@colorado.edu>
2014-02-23 23:38:39 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Saasbook::Application.config.secret_key_base = 'a9cffdba964ac2970b35be07e2850817fa0adb88f17635cff134ae9ef547c0fedcf5947c9e52c2a32cd09a12aef2771eca9d6ea73fd2853791c43dfafea736a6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Saasbook::Application.config.session_store :cookie_store, key: '_Saasbook_session'
